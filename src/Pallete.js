2107083a8de559ce3901e7c1e352585e633a612a
argadipa <argadipapw@gmail.com>
2020-02-19 15:36:16 +0700

    added pallete.

---
+++ src/Pallete.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Pallete extends Component {
+    render () {
+        return (
+            <div className='Pallete'>
+                {/*Nav bar goes here */}
+                <div className='Pallete-colors'>
+                    {/* bunch of color boxes */}
+                </div>
+                {/* footer */}
+            </div>
+        );
+    }
+}
+
+export default Pallete;
