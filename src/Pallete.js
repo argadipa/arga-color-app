833c140aedb0aa78944039a73a07c59a314b2e03
argadipa <argadipapw@gmail.com>
2020-02-20 15:16:37 +0700

    Add working but unstyled slider.

--- src/Pallete.js
+++ src/Pallete.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Pallete.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Pallete extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            level: 500
+        }
+    }
+
+    changeLevel = (level) => {
+        this.setState({ level });
+    }
+
     render () {
-        const colorBoxes = this.props.colors.map(color => {
-            return <ColorBox background={color.color} name={color.name}/>
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+
+        const colorBoxes = colors[level].map(color => {
+            return <ColorBox background={color.hex} name={color.name}/>
         });
 
         return (
             <div className='Pallete'>
+                <Slider 
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel}
+                />
                 {/*Nav bar goes here */}
                 <div className='Pallete-colors'>
                     {/* bunch of color boxes */}
