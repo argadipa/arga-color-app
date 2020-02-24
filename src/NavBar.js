fa3acccca127243478ca784effebaa9121fea02a
argadipa <argadipapw@gmail.com>
2020-02-24 15:31:14 +0700

    Add styled selector.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
-import Select from '@material-ui/core/Select';
-import  MenuItem from "@material-ui/core/MenuItem";
-import  Slider from 'rc-slider';
+import React, { Component } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import './NavBar.css';
-
+import "./NavBar.css";
+import IconButton from "@material-ui/core/IconButton";
 
 class NavBar extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            format: 'hex'
+        this.state = {
+            format: "hex",
+            open: false
         };
     }
 
-    handleChange = (e) => {
+    handleFormatChange = e => {
         this.setState({
-            format: e.target.value
+            format: e.target.value,
+            open: true
         });
         this.props.handleChange(e.target.value);
-    }
+    };
+
+    closeSnackBar = () => {
+        this.setState({
+            open: false
+        });
+    };
 
     render() {
-        
         const { level, changeLevel, handleChange } = this.props;
-        const { format } = this.state;
+        const { format, open } = this.state;
 
         return (
             <header className="Navbar">
                 <div className="logo">
                     <a href="#">ACP</a>
                 </div>
-                <div className="slider-container">
+                <div>
                     <span>Level: {level}</span>
                     <div className="slider">
                         <Slider
@@ -44,12 +53,40 @@ class NavBar extends Component {
                     </div>
                 </div>
                 <div className="select-container">
-                    <Select value={format} onChange={this.handleChange}>
-                        <MenuItem defaultValue value="hex">HEX - #ffffff</MenuItem>
+                    <Select value={format} onChange={this.handleFormatChange}>
+                        <MenuItem defaultValue value="hex">
+                            HEX - #ffffff
+                        </MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
+                        <MenuItem value="rgba">
+                            RGBA - rgba(255,255,255,1)
+                        </MenuItem>
                     </Select>
                 </div>
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                    open={open}
+                    autoHideDuration={3000}
+                    message={
+                        <span id="message-id">
+                            Format changed to {format.toUpperCase()}!
+                        </span>
+                    }
+                    ContentProps={{
+                        "aria-describedby": "message-id"
+                    }}
+                    onClose={this.closeSnackBar}
+                    action={[
+                        <IconButton>
+                            <CloseIcon
+                                onClick={this.closeSnackBar}
+                                color="white"
+                                key="close"
+                                aria-label="close"
+                            />
+                        </IconButton>
+                    ]}
+                />
             </header>
         );
     }
