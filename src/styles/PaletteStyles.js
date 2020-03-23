dc29fd66f759e49af025b7fbf7e395bda4e99bd9
argadipa <argadipapw@gmail.com>
2020-03-23 16:33:46 +0700

    Make ColorBox responsive.

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,13 +1,4 @@
-// export default {
-//     Palette: {
-//         height: "100vh",
-//         display: "flex",
-//         flexDirection: "column"
-//     },
-//     colors: {
-//         height: "90%"
-//     }
-// };
+import sizes from './sizes';
 
 export default {
     Palette: {
@@ -46,6 +37,18 @@ export default {
             textTransform: "uppercase",
             border: "none",
             textDecoration: "none"
+		},
+		[sizes.down("lg")]: {
+			width: " 25%",
+			height: "33.3333%"
+		},
+        [sizes.down("md")]: {
+	        width: "50%",
+            height: "20%"
+        },
+        [sizes.down("xs")]: {
+            width: "100%",
+            height: "10%"
         }
     }
 };
